ccaf59563874ba0fc2a01bce5378abf0c453bb16
Fabian Dammekens <fabian.dammekens@cegeka.be>
2018-01-10 13:24:33 +0100

    [IAC] Puppet cegeka dev - stringify_facts false - fix agent settings

--- manifests/agent/setting.pp
+++ manifests/agent/setting.pp
@@ -15,13 +15,18 @@ define puppet::agent::setting( $ensure = 'present', $value = undef ) {
       }
     }
     'present': {
-      if ($value == undef) or (! is_string($value)) {
-        fail("Puppet::Agent::Setting[${title}]: required parameter value must be a non-empty string")
+      if ($value == undef) or ((! is_string($value)) and (! is_bool($value))) {
+        fail("Puppet::Agent::Setting[${title}]: required parameter value must be a non-empty string or boolean")
       }
       else {
+        if is_bool($value) {
+          $real_value = bool2str($value)
+        } else {
+          $real_value = $value
+        }
         augeas { "puppet::agent::${title}":
-          onlyif  => "match ${title}[. = '${value}'] size == 0",
-          changes => "set ${title} ${value}",
+          onlyif  => "match ${title}[. = '${real_value}'] size == 0",
+          changes => "set ${title} ${real_value}",
         }
       }
     }
--- manifests/master/setting.pp
+++ manifests/master/setting.pp
@@ -14,13 +14,18 @@ define puppet::master::setting( $ensure = 'present', $value = undef ) {
       }
     }
     'present': {
-      if ($value == undef) or (! is_string($value)) {
+      if ($value == undef) or ((! is_string($value)) and (! is_bool($value))) {
         fail("Puppet::Master::Setting[${title}]: required parameter value must be a non-empty string")
       }
       else {
+        if is_bool($value) {
+          $real_value = bool2str($value)
+        } else {
+          $real_value = $value
+        }
         augeas { "puppet::master::${title}":
-          onlyif  => "match ${title}[. = '${value}'] size == 0",
-          changes => "set ${title} '${value}'",
+          onlyif  => "match ${title}[. = '${real_value}'] size == 0",
+          changes => "set ${title} '${real_value}'",
         }
       }
     }
