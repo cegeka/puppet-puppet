5c1a489874a889784797440bcff4fcf28952fa6f
Jurgen Verhasselt <jurgen.verhasselt@cegeka.com>
2026-01-13 15:10:54 +0100

    [PUPPET] support openvox to perforce rollback

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,12 +11,14 @@
 # Sample Usage:
 #
 class puppet (
-  $service_ensure   = 'running',
-  $service_enabled  = true,
-  $package_ensure   = 'latest',
-  $package_name     = 'puppet',
-  $puppet_config_ensure = 'present',
-  $puppet_config    = '/etc/puppet/puppet.conf'
+  $service_ensure          = 'running',
+  $service_enabled         = true,
+  $package_ensure          = 'latest',
+  $package_name            = 'puppet',
+  $package_install_enabled = true,
+  $package_install_options = [],
+  $puppet_config_ensure    = 'present',
+  $puppet_config           = '/etc/puppet/puppet.conf'
 ) {
 
   file { $puppet_config :
@@ -26,8 +28,11 @@ class puppet (
     notify => Service['puppet'],
   }
 
-  package { $package_name :
-    ensure => $package_ensure
+  if $package_install_enabled {
+    package { $package_name :
+      ensure          => $package_ensure,
+      install_options => $package_install_options
+    }
   }
 
   service { 'puppet':
